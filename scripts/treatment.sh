c0372ec7e38b82d8b9d2b929588f2c1e692e8c40
DBecker7 <d7becker@gmail.com>
2023-09-20 21:03:57 -0400

    Move folders

--- scripts/treatment.sh
+++ scripts/treatment.sh
@@ -1,10 +1,11 @@
 # First argument should be the location of the SRA Run Table.
+
 echo $1
 accessions=($(awk -F "," '{if (NR !=1) print $1}' ${1}))
 
 # Initialize directories
 mkdir -p data/fastq
-mkdir -p groutput
+mkdir -p data/groutput
 
 for accession in ${accessions[@]}; do
     echo ""
@@ -13,8 +14,8 @@ for accession in ${accessions[@]}; do
     # Check for duplicates
     # Since gzip'd, fasterq-dump doesn't know about existing files
     if [ `ls data/fastq | grep ${accession} | wc -w` = 0 ]; then
-        if [ ! -f "groutput/${accession}.mapped.csv" ]; then
-            if [ ! -f "groutput/${accession}.mapped.csv.gz" ]; then
+        if [ ! -f "data/groutput/${accession}.mapped.csv" ]; then
+            if [ ! -f "data/groutput/${accession}.mapped.csv.gz" ]; then
                 fasterq-dump ${accession} --progress --outdir "data/fastq/"
             fi
         fi
@@ -22,40 +23,40 @@ for accession in ${accessions[@]}; do
     
     echo "Running Minimap2"
     echo ${accession}
-    if [ ! -f "groutput/${accession}.mapped.csv" ]; then
-        if [ ! -f "groutput/${accession}.mapped.csv.gz" ]; then
+    if [ ! -f "data/groutput/${accession}.mapped.csv" ]; then
+        if [ ! -f "data/groutput/${accession}.mapped.csv.gz" ]; then
             # Some fastqs are combined, some are two separate files.
             # Check the separate files first.
             # Also check for gzipped files
             if [ -f "data/fastq/${accession}_1.fastq" ]; then
-                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}_1.fastq" "data/fastq/${accession}_2.fastq"
-                python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}_1.fastq" "data/fastq/${accession}_2.fastq"
+                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}_1.fastq" "data/fastq/${accession}_2.fastq"
+                python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}_1.fastq" "data/fastq/${accession}_2.fastq"
                 echo "Done"
             elif [ -f "data/fastq/${accession}.fastq" ]; then
-                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}.fastq"
-                python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}.fastq"
+                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}.fastq"
+                python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}.fastq"
                 echo "Done"
             elif [ -f "data/fastq/${accession}_1.fastq.gz" ]; then
-                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}_1.fastq.gz" "data/fastq/${accession}_2.fastq.gz"
-                python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}_1.fastq.gz" "data/fastq/${accession}_2.fastq.gz"
+                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}_1.fastq.gz" "data/fastq/${accession}_2.fastq.gz"
+                python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}_1.fastq.gz" "data/fastq/${accession}_2.fastq.gz"
                 echo "Done"
             elif [ -f "data/fastq/${accession}.fastq.gz" ]; then
-                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}.fastq.gz"
-                python gromstolen/minimap2.py -t 2 -p ${accession} -o groutput --nocut --replace "data/fastq/${accession}.fastq.gz"
+                echo python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}.fastq.gz"
+                python gromstolen/minimap2.py -t 2 -p ${accession} -o data/groutput --nocut --replace "data/fastq/${accession}.fastq.gz"
                 echo "Done"
             fi
         fi
 
         # If successful, remove the fastq file(s).
-        if [ `ls groutput | grep ${accession} | wc -l` = 2 ]; then
-            mfile=`ls groutput/${accession}*mapped.csv*`
+        if [ `ls data/groutput | grep ${accession} | wc -l` = 2 ]; then
+            mfile=`ls data/groutput/${accession}*mapped.csv*`
             if (( `wc -l < $mfile` > 50 )); then
                 echo "Minimap2 Successful, removing fastq"
                 fastq_file=`ls data/fastq/${accession}*`
                 rm $fastq_file
             else 
                 echo `wc -l < $mfile`
-                echo "Minimap2 failed, keeping fastq but removing groutput."
+                echo "Minimap2 failed, keeping fastq but removing data/groutput."
                 rm "$mfile"
                 rm "${mfile/mapped/coverage}"
             fi
@@ -63,5 +64,5 @@ for accession in ${accessions[@]}; do
     fi
 done
 
-gzip -q groutput/*
+gzip -q data/groutput/*
 gzip -q data/fastq/*
