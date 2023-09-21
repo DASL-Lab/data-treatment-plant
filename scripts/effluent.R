bd27ca8dbdd9a2c29623fcc4e3ea306a476f9041
DBecker7 <d7becker@gmail.com>
2023-09-21 17:04:19 -0400

    Always verbose, more PRJs included

--- scripts/effluent.R
+++ scripts/effluent.R
@@ -4,23 +4,12 @@ suppressPackageStartupMessages({
     library(lubridate)
 })
 
-# Rscript effluent.R --quiet --freqmin 0.1 PRJNA745177
-# "--quiet" can go anywhere
+# Rscript scripts/effluent.R --freqmin 0.1 PRJNA745177
 # Bioproject accession should be at the end
 # Assumes you've renamed runtables to SraRunTable_[prj].txt
 
 args <- commandArgs(trailingOnly = TRUE)
-verbose <- TRUE
-if ("--quiet" %in% args) {
-    verbose <- FALSE
-    args <- args[-which(args == "--quiet")]
-}
-
-freqmin <- 0.1
-if ("--freqmin" %in% args) {
-    freqmin <- as.numeric(args[which(args == "--freqmin") + 1])
-}
-
+freqmin <- as.numeric(args[which(args == "--freqmin") + 1])
 prj <- args[length(args)]
 if (!length(prj)) prj <- "PRJNA745177"
 
@@ -28,6 +17,7 @@ runtable <- read.csv(here("data", "runtables",
     paste0("SraRunTable_", prj, ".txt")))
 
 # The columns selected in runtable will be kept in the output
+cat("Load and process runtable.\n")
 if (prj == "PRJNA745177") {
     runtable <- filter(runtable, Replicate != 2 | is.na(Replicate)) %>%
         select(sra = Run,
@@ -36,20 +26,52 @@ if (prj == "PRJNA745177") {
             ww_population,
             location = geo_loc_name,
             avg_spot_len = AvgSpotLen,
-            bases = Bases
-            ) %>%
+            bases = Bases,
+            bioproject = BioProject) %>%
         mutate(
             location = sapply(location,
                 function(x) strsplit(x, split = ", ")[[1]][2]),
             ww_population = as.numeric(gsub(",", "", ww_population))
             )
+} else if (prj == "PRJNA819090") {
+    runtable <- runtable %>%
+        mutate(location = sapply(geo_loc_name,
+            function(x) strsplit(x, ", ")[[1]][2])) %>%
+        select(sra = Run,
+            date = Collection_Date,
+            location,
+            isolation_source = Isolation_Source,
+            avg_spot_len = AvgSpotLen,
+            bases = Bases,
+            bioproject = BioProject
+            )
+} else if (prj == "PRJNA715712") {
+    runtable <- runtable %>%
+        select(sra = Run,
+            location = wwtp,
+            avg_spot_len = AvgSpotLen,
+            bases = Bases,
+            bioproject = BioProject,
+            date = Collection_Date
+        ) %>%
+        # Errant files, added manually from out.log
+        filter(sra != "SRR18583185") %>% # fastq has 20 lines
+        filter(sra != "SRR18583239") # fastq has 24 lines
+} else {
+    stop("I don't know how to deal with this BioProject yet.")
 }
 
-# Gather the actual sample data
+cat("Gather all mapped files.\n")
 # Mutations with coverage less than 10 are removed
     # IMPORTANT: They may be re-added later with a count of 0
     # This is a compromise for memory managment.
 for (i in seq_along(runtable$sra)) {
+    cat(paste0("\b\b\b\b\b\b\b\b\b", i, "/", nrow(runtable)))
+    mname <- here("data", "groutput",
+        paste0("", runtable$sra[i], ".mapped.csv.gz"))
+    if (!file.exists(mname)) {
+        mname <- gsub(".gz", "", mname)
+    }
     m <- read.csv(here("data", "groutput",
         paste0("", runtable$sra[i], ".mapped.csv.gz")))
     m <- m[m$coverage > 10, ]
@@ -63,7 +85,7 @@ for (i in seq_along(runtable$sra)) {
     }
 }
 
-# Remove mutations which never achieved a frequency >0.1
+cat("\nRemove mutations which never achieved a frequency > 0.1\n")
 badmuts <- coco %>%
     select(mutation, frequency) %>%
     group_by(mutation) %>%
@@ -74,7 +96,7 @@ coco <- left_join(coco, badmuts, by = "mutation") %>%
     select(-keep) %>%
     rename(sra = run)
 
-# Ensure all sra's have all mutations - including 0s.
+cat("Ensure all sra's have all mutations - including 0s.\n")
 allmuts <- coco %>%
     select(position, label, mutation) %>%
     distinct()
@@ -102,7 +124,7 @@ for (i in seq_along(my_sra)) {
     }
 }
 
-# Put it all together
+cat("Clean up.\n")
 fullcoco <- left_join(fullcoco, runtable, by = "sra")
 
 dir.create(here("data", "processed"),
@@ -112,3 +134,5 @@ write.csv(x = fullcoco,
     file = gzfile(here("data", "processed",
         paste0(prj, "_processed.csv.gz"))),
     row.names = FALSE)
+
+cat("Done.")
