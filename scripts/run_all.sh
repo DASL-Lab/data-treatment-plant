d3d24d3dba6802263b0e01c660d1c8913e9156ba
DBecker7 <d7becker@gmail.com>
2023-09-26 14:07:47 -0400

    Lab notebook and example run

---
+++ scripts/run_all.sh
@@ -0,0 +1,17 @@
+# No shebang - this is more like a lab notebook.
+
+# Finished runs:
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB65603.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA715712.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA741211.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA745177.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA750263.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA819090.txt
+# In progress:
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA735936.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB44932.txt
+
+rm -r data/fastq/reads_fastq # Remove ENA folder (full of empty folders)
+
+# Effluent (all with the same freqmin)
+Rscript scripts/effluent.R --freqmin 0.05 data/runtables/SraRunTable_PRJEB65603.txt,data/runtables/SraRunTable_PRJNA715712.txt,data/runtables/SraRunTable_PRJNA741211.txt,data/runtables/SraRunTable_PRJNA745177.txt,data/runtables/SraRunTable_PRJNA750263.txt,data/runtables/SraRunTable_PRJNA819090.txt
---
+++ scripts/run_example.sh
@@ -0,0 +1,3 @@
+
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA745177.txt
+Rscript scripts/effluent.R data/runtables/SraRunTable_PRJNA745177.txt
