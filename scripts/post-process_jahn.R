e2aa0288fc5058218821084e64ef1011e78444f1
DBecker7 <d7becker@gmail.com>
2024-05-10 15:36:42 -0400

    Process jahn for matrix factorization

--- scripts/post-process_jahn.R
+++ scripts/post-process_jahn.R
@@ -12,24 +12,28 @@ library(provoc)
 
 system.time(
     jahn <- read.csv(here("data", "processed",
-        "PRJEB44932_processed.csv.gz"))
+            "PRJEB44932_processed.csv.gz"))
 ) # 10 minutes or so.
 jahn <- filter(jahn, !is.na(location))
 jahn <- mutate(jahn, date = ymd(date))
 jahn <- filter(jahn, (date > ymd("2021-02-01")) &
-    (date <= ymd("2021-11-01")))
+        (date <= ymd("2021-11-01")))
 
 if (FALSE) { # Choosing a date range when interactive
     library(ggplot2)
     jahnfo %>%
         ggplot() +
-            aes(x = date, colour = location) +
-            geom_density(linewidth = 2) +
-            scale_x_date(date_breaks = "1 month") +
-            theme(axis.text.x = element_text(angle = 90,
+        aes(x = date, colour = location) +
+        geom_density(linewidth = 2) +
+        scale_x_date(date_breaks = "1 month") +
+        theme(axis.text.x = element_text(angle = 90,
                 vjust = 0.5, hjust = 1))
 } # Added a filter statement for 2021-02-01 to 2021-11-01 above
 
+
+
+
+# Weekly totals ---------------------------------
 jahn2 <- jahn %>%
     mutate(week = week(date)) %>%
     group_by(week, location, label) %>%
@@ -42,15 +46,70 @@ maxes <- aggregate(jahn2$frequency,
     FUN = max, na.rm = TRUE)
 bad_muts <- maxes$label[maxes$x < 0.05]
 jahn2 <- filter(jahn2, !label %in% bad_muts)
+jahn2$mutation <- provoc::parse_mutations(jahn2$label)
 
 write.csv(jahn2, here("data/processed/jahn_weekly.csv"))
 
-# ProVoC-ify
-jahn2$mutation <- provoc::parse_mutations(jahn2$label)
-varmat <- filter_varmat(path = "../../constellations", return_df = TRUE)
-jahn2 <- left_join(jahn2, varmat, by = "mutation")
+
+
+
+# ProVoC-ify ------------------------------------
+varmat <- astronomize() |>
+    filter_lineages(c("B.1.617.2", "B.1.1.7", "B.1.351", "P.1"))
+jahn3 <- provoc::fuse(jahn2, varmat)
 
 # Only mutations with known lineages
-jahneage <- jahn2[!is.na(jahn2$B.1.1.7), ]
+jahn3 <- jahn3[!is.na(jahn3$lin_B.1.1.7), ]
 
-write.csv(jahneage, file = "data/processed/jahn_weekly_variants.csv")
+write.csv(jahn3, file = "data/processed/jahn_weekly_variants.csv")
+
+
+
+
+# ProVoC-ify 2: Usher ---------------------------
+varmat <- usher_barcodes(write = FALSE) |>
+    filter_lineages(c("B.1.617.2", "B.1.1.7", "B.1.351", "P.1"))
+jahn3 <- provoc::fuse(jahn2, varmat)
+
+# Only mutations with known lineages
+jahn3 <- jahn3[!is.na(jahn3$lin_B.1.1.7), ]
+
+write.csv(jahn3, file = "data/processed/jahn_weekly_variants.csv")
+
+
+
+
+# Wide (for matrix factorization) ---------------
+good_muts <- tapply(jahn$frequency, jahn$label, max)
+good_mut_names <- names(good_muts[good_muts >= 0.20])
+length(good_mut_names)
+jahn4 <- jahn[jahn$label %in% good_mut_names, ]
+jahn4$mutation <- provoc::parse_mutations(jahn4$label)
+dim(jahn4)
+
+unique(table(jahn4$sra)) # Expect one value (length(good_mut_names))
+jahn4 <- jahn4 %>% arrange(sra, mutation)
+
+jahn_wides <- lapply(X = unique(jahn4$location), function(loc) {
+    loc_temp <- jahn4[jahn4$location == loc, ]
+    sapply(X = unique(loc_temp$sra),
+        FUN = function(sra) {
+            sra_temp <- loc_temp[loc_temp$sra == sra,
+                c("frequency", "mutation")]
+            res <- sra_temp[, "frequency"]
+            names(res) <- sra_temp[, "mutation"]
+            res
+        })
+})
+dim(jahn_wides[[1]])
+rownames(jahn_wides[[1]]) |> head()
+colnames(jahn_wides[[1]]) |> head()
+
+jahn_meta <- jahn4 %>%
+    select(sra, date, location) %>%
+    distinct() %>%
+    as.data.frame()
+attr(jahn_wides, "metadata") <- jahn_meta
+
+saveRDS(jahn_wides,
+    file = here("data", "processed", "jahn_wide_list_attributes.csv"))
