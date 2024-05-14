ac876da3bc0a454577b33897dc72dcb95cbe27a5
DBecker7 <d7becker@gmail.com>
2024-05-14 13:12:08 -0400

    Big one last

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -31,6 +31,6 @@ rm -r data/fastq/reads_fastq # Remove ENA folder (full of empty folders)
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJNA1042787.txt,data/runtables/SraRunTable_PRJNA759260.txt,txt,data/runtables/SraRunTable_PRJNA741211.txt,data/runtables/SraRunTable_PRJNA745177.txt,data/runtables/SraRunTable_PRJNA715712.txt,data/runtables/SraRunTable_PRJEB65603.txt,data/runtables/SraRunTable_PRJNA720687.txt,data/runtables/SraRunTable_PRJNA796340.txt,data/runtables/SraRunTable_PRJNA750263.txt,data/runtables/SraRunTable_PRJNA788395.txt,data/runtables/SraRunTable_PRJNA819090.txt,data/runtables/SraRunTable_PRJEB48206.txt,data/runtables/SraRunTable_PRJNA735936.txt
 # These ones take longer
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB48985.txt
-Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB55313.txt
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJNA1027858.txt
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB44932.txt
+Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB55313.txt
