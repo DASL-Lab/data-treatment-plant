d08148034ca107eaf260321f5d2723f461e92511
DBecker7 <d7becker@gmail.com>
2024-05-27 09:32:15 -0400

    New runtable, rearrange

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,34 +1,33 @@
 # No shebang - this is more like a lab notebook.
-# Running from start to finish would take about two weeks.
+# Running from start to finish would take about two-three weeks.
 
-
-# Finished runs (ordered approximately by runtime; low-high):
+# Finished runs (ordered low-high by post-processed file size)
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA811594.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA1042787.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA759260.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA745177.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA811594.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA741211.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB65603.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA715712.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA720687.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA719837.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA796340.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA731975.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA772783.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA750263.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA796340.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA788395.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA1027858.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA819090.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB48206.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA735936.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB48985.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA1027858.txt
 bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB44932.txt
 
-# Unknown runtimes (Loooooong)
+
+# Unknown runtimes (Long or not yet processed)
 #bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA661613.txt # minimap2.py failed.
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB55313.txt # Downloaded, not processed.
-# In progress
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA772783.txt
-bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA731975.txt
-#bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA764181.txt #clinical data!!!
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB55313.txt # Downloaded, not processed (memory issues).
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJEB48985.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA719837.txt
+bash scripts/treatment.sh data/runtables/SraRunTable_PRJNA1088471.txt
 
 rm -r data/fastq/reads_fastq # Remove ENA folder (full of empty folders)
 
@@ -42,3 +41,6 @@ Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJNA1027858.txt
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB44932.txt
 Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJEB55313.txt
+
+# Running
+Rscript scripts/effluent.R --freqmin 0.1 --min_coverage 40 --beep data/runtables/SraRunTable_PRJNA1088471.txt
