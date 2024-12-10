72f1e5c86f5343bba056a2814eb9d62d15facfc6
Devan Becker <d7becker@gmail.com>
2024-12-10 16:20:49 -0500

    Better plots

--- scripts/overton_data_postprocessing.R
+++ scripts/overton_data_postprocessing.R
@@ -2,6 +2,7 @@ library(here)
 library(dplyr)
 library(lubridate)
 library(readr)
+library(ggplot2)
 
 overton <- read.csv(
     here(
@@ -30,6 +31,24 @@ system.time(
 head(toronto_wwtps)
 write_csv(toronto_wwtps, here("data/processed/overton_toronto.csv.gz"))
 
+barcodes <- provoc::usher_barcodes()
+toronto_wwtps |>
+    filter(str_starts(location, "Y1")) |>
+    group_by(location, mutation) |>
+    mutate(
+        lower_thresh = sum(frequency > 0.2) > 2,
+        upper_thresh = mean(frequency < 0.9) > 0.2
+    ) |>
+    ungroup() |>
+    filter(lower_thresh, upper_thresh) |>
+    select(date, frequency, mutation) |>
+    distinct() |>
+    ggplot() +
+    aes(x = date, y = frequency, group = mutation) +
+    geom_smooth(se = FALSE, formula = y ~ x, method = "loess",
+        colour = 1, alpha = 0.001) +
+    ylim(c(0, 1))
+
 highland <- overton |>
     filter(str_starts(location, "Highland")) |>
     mutate(
@@ -45,6 +64,7 @@ highland <- overton |>
     mutate(
         frequency = count / (coverage + 1)
     )
+
 
 head(highland)
 write.csv(highland, here("data/processed/highland.csv"))
