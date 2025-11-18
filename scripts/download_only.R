99adf9ffc6923324ca1aa3c5475849dd3deafaa5
Devan Becker <d7becker@gmail.com>
2025-11-18 16:43:18 -0500

    gzip first then count lines, bugfix, love

--- scripts/download_only.R
+++ scripts/download_only.R
@@ -24,13 +24,13 @@ runtable$length_2 <- NA
 
 loop_times <- c()
 t0 <- Sys.time()
-for (i in 1:5){#seq_len(nrow(runtable))) {
+for (i in seq_len(nrow(runtable))) {
 	t1 <- Sys.time()
 	cat("Run", i, "of", nrow(runtable), "\b. ")
 	cat("Downloading. ")
 	fasterq_dump <- system2(
 		"fasterq-dump",
-		c(runtable$Run[i], "--progress", "-Zf", "--outdir", outdir),
+		c(runtable$Run[i], "--progress", "-Z", "--outdir", outdir),
 		stdout = TRUE, stderr = TRUE
 	)
 
@@ -42,22 +42,27 @@ for (i in 1:5){#seq_len(nrow(runtable))) {
 	runtable$reads_read[i]  <- str_extract(fasterq_dump, "reads read.*") |>
 		readr::parse_number()
 
-	runtable$length_1[i] <- system2("wc",
-		c("-l", paste0(outdir, "/", runtable$Run[i], "_1.fastq")),
-		stdout = TRUE
-	) |>
-		readr::parse_number()
-	runtable$length_2[i] <- system2("wc",
-		c("-l", paste0(outdir, "/", runtable$Run[i], "_2.fastq")),
-		stdout = TRUE
-	) |>
-		readr::parse_number()
-
 	cat("Gzipping. ")
 	system2("gzip", 
 		c("-f", paste0(outdir, "/", runtable$Run[i], "_1.fastq")))
 	system2("gzip", 
 		c("-f", paste0(outdir, "/", runtable$Run[i], "_2.fastq")))
+
+
+	runtable$length_1[i] <- paste(
+		"gunzip -c", 
+		paste0(outdir, "/", runtable$Run[i], "_1.fastq"),
+		"| wc -l"
+	) |>
+		system(intern = TRUE) |>
+		as.numeric()
+	runtable$length_2[i] <- paste(
+		"gunzip -c", 
+		paste0(outdir, "/", runtable$Run[i], "_2.fastq"),
+		"| wc -l"
+	) |>
+		system(intern = TRUE) |>
+		as.numeric()
 
 	cat("Minimapping. ")
 	minimap2 <- system2(
@@ -75,15 +80,17 @@ for (i in 1:5){#seq_len(nrow(runtable))) {
 		stdout = TRUE, stderr = TRUE
 	)
 
-	lastmap <- minimap2[length(minimap2)]
-	lastmap <- lastmap |> str_split_1("\\\r")
-	lastmap	<- lastmap[length(lastmap)]
-	runtable$floor_50000[i] <- lastmap |> readr::parse_number()
-	lastmap <- str_remove(lastmap, "[\\d.]+")
-	runtable$mapped_50000[i] <- readr::parse_number(lastmap)
-	lastmap <- str_remove(lastmap, "[\\d.]+")
-	runtable$mapped_perc[i] <- readr::parse_number(lastmap)
-
+	if (length(minimap2) != 0) {
+		lastmap <- minimap2[which.max(sapply(minimap2, nchar))]
+		lastmap <- str_remove(lastmap, "\033.*")
+		lastmap <- lastmap |> str_split_1("\\\r")
+		lastmap	<- lastmap[length(lastmap)]
+		runtable$floor_50000[i] <- lastmap |> readr::parse_number()
+		lastmap <- str_remove(lastmap, "[\\d.]+")
+		runtable$mapped_50000[i] <- readr::parse_number(lastmap)
+		lastmap <- str_remove(lastmap, "[\\d.]+")
+		runtable$mapped_perc[i] <- readr::parse_number(lastmap)
+	}
 	coverage_file <- here("data", "groutput", 
 		paste0(runtable$Run[i], ".coverage.csv"))
 	coverage <- read.csv(coverage_file)
@@ -91,23 +98,22 @@ for (i in 1:5){#seq_len(nrow(runtable))) {
 	runtable$avg_depth[i] <- mean(coverage$coverage)
 
 	loop_times[i] <- difftime(Sys.time(), t1, units = "mins")
-	if (i > 2) {
-		loop_ind <- seq_len(length(loop_times))
-		loop_pred <- lm(loop_times ~ loop_ind) |>
-			predict(newdata = list(loop_ind = nrow(runtable)))
-		cat("Done in ", loop_times[i], "mins. ")
-		cat(
-			"Predicted to finish in",
-			round(loop_pred - sum(loop_times)),
-			"mins."
-		)
-	}
+	loop_times_mean <- mean(loop_times, na.rm = TRUE)
+	loops_left <- nrow(runtable) - i
+	loop_pred <- loops_left * loop_times_mean
+	cat("Done in ", loop_times[i], "mins. ")
+	cat(
+		"Predicted to finish in",
+		round(loop_pred - sum(loop_times), 2),
+		"mins."
+	)
 	cat("\n")
 }
 head(runtable)
 
 write.csv(runtable,
-here("data/saved_fastq", runfile_out),
-row.names = FALSE)
+	here("data/saved_fastq", runfile_out),
+	row.names = FALSE
+)
 
 
