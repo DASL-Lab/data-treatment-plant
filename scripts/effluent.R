acd553176e3de1599dc4f9c58a2c17b21ee99f07
Devan Becker <d7becker@gmail.com>
2026-04-17 10:30:44 -0400

    bugfixes

--- scripts/effluent.R
+++ scripts/effluent.R
@@ -348,7 +348,7 @@ get_runtable <- function(prj) {
             rename(location = geo_loc_name)
     } else if (prj == "PRJNA1365088") {
         runtable <- runtable |>
-            rename(location = "aircraft")
+            mutate(location = "aircraft")
     } else {
         stop("I don't know how to deal with this BioProject yet.")
     }
@@ -460,6 +460,11 @@ add_missing_mutations <- function(coco) {
                 cfile <- here("data", "groutput",
                     paste0(my_sra[i], ".coverage.csv"))
             }
+            if (!file.exists(cfile)) {
+                warning("mapped file exists, but not coverage. Removing mutations from data.")
+                coco <- coco[coco$sra != my_sra[i], ]
+                next
+            }
             c <- read.csv(cfile)
             missings <- anti_join(allmuts, mi, by = "label")
             missings$count <- 0
@@ -501,7 +506,7 @@ if (grepl(",", argv$BioProject))
     argv$BioProject <- strsplit(argv$BioProject, ",")[[1]]
 
 if (FALSE) {
-    argv <- list(BioProject = "data/runtables/SraRunTable_PRJNA887942.txt",
+    argv <- list(BioProject = "data/runtables/SraRunTable_PRJNA745177.txt",
         beep = TRUE, parse_mutations = FALSE,
         freqmin = 0.1, min_coverage = 40)
 }
